4962d7be5079acee10350dd7a4cea89636384f57
Kin <riennsuu@gmail.com>
2024-05-12 04:57:03 +0700

    chore(utils): added command options resolver utilization (#2)
    * utils: added command options resolver
    
    * fix: optional option handler
    
    * chore(ship): separate ship function and use `getUserOption` util

--- src/commands/ship.ts
+++ src/commands/ship.ts
@@ -1,14 +1,15 @@
 import { type ChatInputCommand } from "../types.d.ts";
 import {
-	type APIApplicationCommandInteractionDataUserOption,
+	type API,
+	type APIChatInputApplicationCommandGuildInteraction,
 	type APIInteractionResponseChannelMessageWithSource,
 	type APIInteractionResponseDeferredChannelMessageWithSource,
 	ApplicationCommandOptionType,
 	InteractionResponseType,
 } from "@djs/core";
 import { createCanvas, loadImage } from "canvas";
-import { fetch as sapphireFetch, FetchResultTypes } from "@sapphire/fetch";
 import {
+	getUserOption,
 	isGuildChatInputCommandInteraction,
 	userAvatar,
 } from "../utils/mod.ts";
@@ -39,75 +40,7 @@ export default {
 				} satisfies APIInteractionResponseChannelMessageWithSource,
 			);
 		} else {
-			queueMicrotask(async () => {
-				const percentage = Math.floor(Math.random() * 101);
-				const match = Math.floor(percentage / 25);
-
-				const matchMessages = [
-					"Looks like it's not meant to be",
-					"Seems like a bit of mismatch, huh?",
-					"Not quite hitting the mark, huh?",
-					"Let's give it a shot, maybe they'll end up together!",
-					"You two are a perfect match!",
-				];
-
-				const canvas = createCanvas(2160, 720);
-				const context = canvas.getContext("2d");
-				const heart = await loadImage(
-					`./assets/heart${match}.png`,
-				);
-				const targetId = interaction.data.options?.find((ctx) =>
-					ctx.name === "target"
-				) as APIApplicationCommandInteractionDataUserOption;
-				const someoneId = interaction.data.options?.find((ctx) =>
-					ctx.name === "someone"
-				) as APIApplicationCommandInteractionDataUserOption;
-				const target =
-					interaction.data.resolved!.users![targetId.value];
-				const someone = someoneId
-					? interaction.data.resolved!.users![someoneId.value]
-					: interaction.member.user;
-				const targetAvatar = await loadImage(
-					await sapphireFetch(
-						userAvatar(api.rest.cdn, target),
-						FetchResultTypes.Buffer,
-					),
-				);
-				const someoneAvatar = await loadImage(
-					await sapphireFetch(
-						userAvatar(api.rest.cdn, someone),
-						FetchResultTypes.Buffer,
-					),
-				);
-
-				context.drawImage(heart, 720, 0, 720, 720);
-
-				context.beginPath();
-				context.moveTo(360, 360);
-				context.arc(360, 360, 360, 0, Math.PI * 2, true);
-				context.moveTo(1800, 360);
-				context.arc(1800, 360, 360, 0, Math.PI * 2, true);
-				context.closePath();
-				context.clip();
-
-				context.drawImage(targetAvatar, 0, 0, 720, 720);
-				context.drawImage(someoneAvatar, 1440, 0, 720, 720);
-
-				await api.interactions.editReply(
-					interaction.application_id,
-					interaction.token,
-					{
-						content:
-							`**${target.username}** and **${someone.username}** are ${percentage}% match! 💞\n${
-								matchMessages[match]
-							}`,
-						files: [{
-							name: "ship.png",
-							data: await canvas.toBuffer(),
-						}],
-					},
-				);
-			});
+			queueMicrotask(() => ship(api, interaction));
 
 			return Response.json(
 				{
@@ -118,3 +51,72 @@ export default {
 		}
 	},
 } satisfies ChatInputCommand;
+
+async function ship(
+	api: API,
+	interaction: APIChatInputApplicationCommandGuildInteraction,
+) {
+	const percentage = Math.floor(Math.random() * 101);
+	const match = Math.floor(percentage / 25);
+
+	const matchMessages = [
+		"Looks like it's not meant to be",
+		"Seems like a bit of mismatch, huh?",
+		"Not quite hitting the mark, huh?",
+		"Let's give it a shot, maybe they'll end up together!",
+		"You two are a perfect match!",
+	];
+
+	const canvas = createCanvas(2160, 720);
+	const context = canvas.getContext("2d");
+
+	const target = getUserOption(interaction, "target", true);
+	const someone = getUserOption(interaction, "someone") ??
+		interaction.member.user;
+
+	context.drawImage(
+		await loadImage(`./assets/heart${match}.png`),
+		720,
+		0,
+		720,
+		720,
+	);
+
+	context.beginPath();
+	context.moveTo(360, 360);
+	context.arc(360, 360, 360, 0, Math.PI * 2, true);
+	context.moveTo(1800, 360);
+	context.arc(1800, 360, 360, 0, Math.PI * 2, true);
+	context.closePath();
+	context.clip();
+
+	context.drawImage(
+		await loadImage(userAvatar(api.rest.cdn, target)),
+		0,
+		0,
+		720,
+		720,
+	);
+	context.drawImage(
+		await loadImage(userAvatar(api.rest.cdn, someone)),
+		1440,
+		0,
+		720,
+		720,
+	);
+
+	await api.interactions.editReply(
+		interaction.application_id,
+		interaction.token,
+		{
+			content:
+				`**${target.username}** and **${someone.username}** are ${percentage}% match! 💞\n${
+					matchMessages[match]
+				}`,
+			files: [{
+				name: "ship.png",
+				data: await canvas.toBuffer(),
+			}],
+		},
+	);
+}
--- src/utils/mod.ts
+++ src/utils/mod.ts
@@ -1,3 +1,5 @@
 export * from "./avatar.ts";
 export * from "./commands.ts";
 export * from "./interactions.ts";
+
+export * from "./command_options_resolver.ts";
